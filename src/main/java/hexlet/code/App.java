ea32a54f22e8704c9b5b4327a211dce8966f19c7
FCWorkSpace <c.igor@list.ru>
2022-09-14 19:37:24 +0700

    step 2 - for first deploy

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,27 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+import io.javalin.core.JavalinConfig;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Javalin app = getApp();
+        app.start(getPort());
     }
+
+    private static Javalin getApp() {
+        Javalin app = Javalin.create(JavalinConfig::enableDevLogging);
+        addRoutes(app);
+        return app;
+    }
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "3000");
+        return Integer.parseInt(port);
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", ctx -> ctx.result("Hello world!"));
+    }
+
 }
